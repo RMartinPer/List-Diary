7b8c14a1b38d5bf78aa463112c39d59db041ad0e
Ru95Gasol <raumar95gmail.com>
2020-12-27 20:54:53 +0100

    Updated the Privacy Policy pages and its script and added the portuguese version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+var language = window.navigator.language;
+var languageFirstTwo = language.substr(0, 2);
+
 openLink = function() {
-	var language = window.navigator.language;
-	var languageFirstTwo = language.substr(0, 2);
 	var redirectLink;
 	switch (languageFirstTwo) {
 		case "es":
@@ -8,6 +9,9 @@ openLink = function() {
 		break;
 		case "fr":
 			redirectLink = "privacy-policy-fr.html";
+		break;
+		case "pt":
+			redirectLink = "privacy-policy-pt.html";
 		break;
 		default:
 			redirectLink = "privacy-policy-en.html";
@@ -18,8 +22,26 @@ openLink = function() {
 openEmail = function() {
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ||
 	/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform)) {
-		window.open("mailto:raumar95@gmail.com", "_blank");
+		window.location.href = "mailto:raumar95@gmail.com";
 	} else {
-		window.open("https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=raumar95@gmail.com", "_blank");
+		var confirmGmail;
+		switch (languageFirstTwo) {
+			case "es":
+				confirmGmail = "¿Quieres enviar un correo electrónico desde tu cuenta de Gmail?";
+			break;
+			case "fr":
+				confirmGmail = "Voulez-vous envoyer un e-mail à partir de votre compte Gmail?";
+			break;
+			case "pt":
+				confirmGmail = "Você deseja enviar um e-mail da sua conta do Gmail?";
+			break;
+			default:
+				confirmGmail = "Do you want to send an email from your Gmail account?";
+		}
+		if (window.confirm(confirmGmail)) {
+			window.open("https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=raumar95@gmail.com", "_blank");
+		} else {
+			window.location.href = "mailto:raumar95@gmail.com";
+		}
 	}
 }
